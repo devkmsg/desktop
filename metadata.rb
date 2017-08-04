4dbe935eff3fe2d2aa7a5e9e91cb999db66ed3d2
Andrew Thompson <netengr2009@gmail.com>
2017-08-04 15:17:29 -0400

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name 'desktop'
+maintainer 'Andrew Thompson'
+maintainer_email 'netengr2009@gmail.com'
+license 'all_rights'
+description 'Installs/Configures Mac desktop'
+long_description 'Installs/Configures Mac desktop'
+version '0.1.0'
+
+depends 'ruby_rbenv', '~> 2.0.3'
---
+++ recipes/default.rb
@@ -0,0 +1,9 @@
+#
+# Cookbook Name:: desktop
+# Recipe:: default
+#
+# Copyright (c) 2017 The Authors, All Rights Reserved.
+
+rbenv_global '2.4.4'
+rbenv_ruby '2.3.3'
+
